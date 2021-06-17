2e4ea8c8284097ba38929f464654349743650a3b
H.M.E <1316126217@qq.com>
2021-06-17 12:44:06 +0800

    add  removeNthFromEnd
    删除链表的倒数第N个节点

---
+++ 19.删除链表的倒数第N个节点.js
@@ -0,0 +1,15 @@
+var removeNthFromEnd = function (head, n) {
+  let dummy = new ListNode();
+  dummy.next = head;
+  let n1 = dummy;
+  let n2 = dummy;
+  for (let i = 0; i <= n; i++) {
+    n2 = n2.next;
+  }
+  while (n2 !== null) {
+    n1 = n1.next;
+    n2 = n2.next;
+  }
+  n1.next = n1.next.next;
+  return dummy.next;
+}
