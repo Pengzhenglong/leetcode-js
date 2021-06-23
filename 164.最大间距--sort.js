6dc154e96ffb1e1cbcf2ad034dc34a9eda7d8d28
H.M.E <1316126217@qq.com>
2021-06-23 11:17:59 +0800

    优化

--- 164.最大间距--sort.js
+++ 164.最大间距--sort.js
@@ -13,9 +13,9 @@ nums=[3,6,9,1]
 var maximumGap = function (nums, max = 0) {
   if (nums.length < 2){
     return 0;}
-  nums.sort((a, b) => {
+  nums.sort((a, b) => 
     a - b
-  })
+  )
   for (let i = 0; i < nums.length - 1; i++) {
     max = Math.max(max, nums[i + 1] - nums[i])
   }
