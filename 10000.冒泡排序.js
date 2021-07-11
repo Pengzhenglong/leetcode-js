348e0ce5ddb850a2d8cea19907a7b2566ead8ba8
H.M.E <1316126217@qq.com>
2021-07-11 22:38:17 +0800

    冒泡排序

--- 10000.冒泡排序.js
+++ 10000.冒泡排序.js
@@ -1,12 +1,4 @@
-# leetcode-js
 
-### Js实现冒泡排序
-
-冒泡排序是效率最低的排序算法，由于算法嵌套了两轮循环，所以时间复杂度委O(n^2)
-
-最好的情况给出一个已经排序的数组进行冒泡排序，时间复杂度为O(n)
-
-```
 // 冒泡排序
 function bubbleSort(arr) {
   let len = arr.length;
@@ -22,4 +14,3 @@ function bubbleSort(arr) {
   }
   return arr
 }
-```
