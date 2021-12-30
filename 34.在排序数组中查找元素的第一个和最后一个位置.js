57cfc68df05616a2d1d28ff5747b4d63fc90c7ca
H.M.E <1316126217@qq.com>
2021-12-30 21:07:15 +0800

    add 二分法--34查找第一个元素和最后一个元素

--- 34.在排序数组中查找元素的第一个和最后一个位置.js
+++ 34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -10,8 +10,34 @@
  * @param {number} target
  * @return {number[]}
  */
+
+//  方法1:先二分，在寻找左右边界
+//  思路：二分查找，然后向左和向右尝试找相同的元素
+//  复杂度：时间复杂度O(n)，空间复杂度O(1)
+//nums = [5,7,7,8,8,10], target = 8
+
+// 一行代码解决
+// return [nums.indexOf(target), nums.lastIndexOf(target)]
+
+
 var searchRange = function (nums, target) {
-  return [nums.indexOf(target), nums.lastIndexOf(target)]
+
+  let left = 0, right = nums.length - 1, mid;
+  while (left <= right) {
+    //二分查找target
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) break;
+    if (nums[mid] > target) right = mid - 1;
+    else left = mid + 1;
+
+  }
+  if (left > right) {
+    return [-1, -1]
+  }
+  let i = mid, j = mid;
+  while (nums[i] === nums[i - 1]) i--;  //向左尝试找相同的元素
+  while (nums[j] === nums[j + 1]) j++;  //向右尝试找相同的元素
+  return [i, j]
 };
-// @lc code=end
+  // @lc code=end
 
