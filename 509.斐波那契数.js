169043b519bc951c38b9e00151ff136a4be8a1ce
H.M.E <1316126217@qq.com>
2022-01-24 12:20:29 +0800

    add  简单递归

---
+++ 509.斐波那契数.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2)
+};
+// @lc code=end
+
