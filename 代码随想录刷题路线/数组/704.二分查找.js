5c9682854a850acdb18d81a255fba484fe5d4360
H.M.E <1316126217@qq.com>
2022-05-23 18:39:03 +0800

    add 二分查找

---
+++ 代码随想录刷题路线/数组/704.二分查找.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=javascript
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// var search = function (nums, target) {
+//   return nums.indexOf(target)
+// };
+// 二分查找法
+var search = function (nums, target) {
+  let left = 0
+  let right = nums.length - 1
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] > target) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+  return -1
+};
+// @lc code=end
+
