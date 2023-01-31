4cbb7709116201fbce2dc69e339b4af57b0f8356
H.M.E <1316126217@qq.com>
2023-01-31 14:40:42 +0800

    feat: 添加手写深拷贝

--- 前端手写题/手写深拷贝.js
+++ 前端手写题/手写深拷贝.js
@@ -1,19 +1,39 @@
 const _sampleDeepClone = target => {
     // base case
-      if (target == null || typeof target !== 'object') {
-          return target;
-      }
-      const cloneTarget = Array.isArray(target) ? [] : {};
-      for (let key in target) {
-          if (target.hasOwnProperty(key)) {
-              if (target[key] && typeof target[key] == 'object') {
+    if (target == null || typeof target !== 'object') {
+        return target;
+    }
+    const cloneTarget = Array.isArray(target) ? [] : {};
+    for (let key in target) {
+        if (target.hasOwnProperty(key)) {
+            if (target[key] && typeof target[key] == 'object') {
                 // 递归
-                  cloneTarget[key] = _sampleDeepClone(target[key]);
-              } else {
-                  // 基本数据类型
-                  cloneTarget[key] = target[key];
-              }
-          }
-      }
-      return cloneTarget;
-  };
+                cloneTarget[key] = _sampleDeepClone(target[key]);
+            } else {
+                // 基本数据类型
+                cloneTarget[key] = target[key];
+            }
+        }
+    }
+    return cloneTarget;
+};
+
+
+
+const sampleDeepClone = target => {
+    if (target === null || typeof target !== 'object') {
+        return target;
+    }
+    const cloneTarget = Array.isArray(target) ? [] : {};
+    for (let key in target) {
+        if (target.hasOwnProperty(key)) {
+            if (target[key] && typeof target[key] === 'object') {
+                // 递归
+                cloneTarget[key] = sampleDeepClone(target[key]);
+            } else {
+                cloneTarget[key] = target[key];
+            }
+        }
+    }
+    return  cloneTarget;
+}
