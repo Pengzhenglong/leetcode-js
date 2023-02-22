31fde8f7c93222903a0faae65cc60f8b4b750dd0
H.M.E <1316126217@qq.com>
2023-02-22 14:18:13 +0800

    feat: 添加160链表相交

---
+++ 代码随想录刷题路线/链表/7.环形链表/142.环形链表II.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=javascript
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// 两种循环实现方式
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// 先判断是否是环形链表
+var detectCycle = function(head) {
+
+};
+
+var detectCycle = function(head) {
+
+};
