1803db9d1c1e8d42e5e3f083b6cad28d3d5acd75
H.M.E <1316126217@qq.com>
2023-02-28 17:39:43 +0800

    feat: 添加赎金信

--- 代码随想录刷题路线/哈希表/6.赎金信.js/383. 赎金信.js
+++ 代码随想录刷题路线/哈希表/6.赎金信.js/383. 赎金信.js
@@ -25,3 +25,15 @@ var canConstruct = function(ransomNote, magazine) {
 };
 // @lc code=end
 
+var canConstruct = function(ransomNote, magazine) {
+   const  setArray=  new  Array(26).fill(0);
+   const  base  = 'a'.charCodeAt();
+   for(const  i  of  magazine){
+    setArray[i.charCodeAt()-base]++;
+   } 
+   for(const  i  of  ransomNote){
+    if(!setArray[i.charCodeAt()-base]) return false;
+    setArray[i.charCodeAt()-base]--;
+   }
+   return  true;
+};
